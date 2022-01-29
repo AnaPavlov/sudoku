b536b900c84e6310f9641a7e4e50c5597d4883ab
Ana Pavlovic <apavlovic@vast.com>
2022-01-29 13:24:11 +0100

    Add table

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Table from './src/components/Table';
+import {generateDummyTable, generateTable} from './src/gameLogic/index';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [gameStatus, setGameStatus] = useState(false);
+    const [table, setTable] = useState(generateDummyTable());
+
+    const handleGameStart = () => {
+        setGameStatus(true);
+        const newTable = generateTable();
+        setTable(newTable);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Sudoku</Text>
+            <View style={styles.tableWrapper}>
+                <Table table={table}/>
+            </View>
+            <Button
+                onPress={handleGameStart}
+                disabled={gameStatus}
+                title="Start new game"
+                color="#F97900"
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#46301B',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+    tableWrapper: {
+        margin: 15,
+    },
 });
